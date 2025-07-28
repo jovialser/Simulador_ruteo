57db6711bb9f1b9c97186efeee07f12fa02542ce
Sergio Sánchez <49963862+jovialser@users.noreply.github.com>
2025-07-28 11:41:37 -0300

    Volvemos a version local

---
+++ frontend/src/data/constants.js
@@ -0,0 +1,12 @@
+// c:\Users\Usuario\Downloads\Simulador_Ambulancias_cityBA-main\Simulador_Ambulancias_cityBA-main\frontend\src\data\constants.js
+
+// 📍 Centros médicos por municipio
+export const centrosMedicos = {
+  "Hospital General de Agudos Dr. Cosme Argerich (CABA)": [-34.622, -58.364],
+  "Hospital Interzonal General de Agudos Evita (Lanús)": [-34.706, -58.398],
+  "Hospital Zonal General de Agudos Dr. Isidoro Iriarte (Quilmes)": [-34.720, -58.264],
+  "Hospital Interzonal General de Agudos Eva Perón (San Martín)": [-34.575, -58.552],
+  "Hospital Interzonal de Agudos Dr. Alberto Balestrini (La Matanza)": [-34.671, -58.565]
+};
+
+export const municipios = Object.keys(centrosMedicos);
