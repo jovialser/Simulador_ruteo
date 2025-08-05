368c06f09d39082311c05af7e6e041033e22174c
Sergio Sánchez <49963862+jovialser@users.noreply.github.com>
2025-08-05 10:56:44 -0300

    Update SimuladorForm.jsx

--- frontend/src/components/SimuladorForm.jsx
+++ frontend/src/components/SimuladorForm.jsx
@@ -219,7 +219,7 @@ export default function SimuladorForm({ onCoordenadasSeleccionadas }) {
           <h2>🟢 Resultado (Tradicional)</h2>
           <p>Ambulancia: <strong>{resultado.ambulancia}</strong></p>
           <p>ETA: <strong>{resultado.eta_minutos} minutos</strong></p>
-          <p>Zona: {resultado.zona}</>
+          <p>Zona: {resultado.zona}</p>
             <p>Tipo de vía: {resultado.tipo_via}</p>
           </div>
   );
